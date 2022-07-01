5611bd4cf5a8d124960de6697a928d902d0e7018
YasHabib <yasin_habib@outlook.com>
2022-06-30 23:41:13 -0600

    Updated program.cs

---
+++ MarketPlaceForYou/MarketPlaceForYou.Api/SwashBuckle/AuthHeaderOperationFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace MarketPlaceForYou.Api.SwashBuckle
+{/// <summary>
+/// Code knows how to use JSON token....something...
+/// </summary>
+    public class AuthHeaderOperationFilter: IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            // Only authorize the endpoint if it has an Authorize attribute
+            if (context.MethodInfo.DeclaringType == null)
+                return;
+            var isAuthorized = context.MethodInfo.DeclaringType.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any() ||
+                               context.MethodInfo.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any();
+            if (!isAuthorized) return;
+
+            // Add the Access Token parameter to the endpoint documentation
+            if (operation.Security == null)
+                operation.Security = new List<OpenApiSecurityRequirement>();
+
+            var scheme = new OpenApiSecurityScheme { Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearer" } };
+            operation.Security.Add(new OpenApiSecurityRequirement
+            {
+                [scheme] = new List<string>()
+            });
+        }
+    }
+}
